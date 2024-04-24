d4df6c94bc2c14979b9a4b735ddd710c2b45d2f9
Alishba Basharat <166277441+AlishbaBasharat@users.noreply.github.com>
2024-04-24 08:57:54 -0700

    created currency converter app

---
+++ index.ts
@@ -0,0 +1,34 @@
+import inquirer from "inquirer";
+
+let currency: any = {
+    USD: 1,
+    EUR: 0.91,
+    GBP: 0.76,
+    INR: 74.57,
+    PKR: 280
+};
+let useranswer = await inquirer.prompt([{
+    name: "fromcurrency",
+    message: "Enter the currency that you want to change from",
+    type: "list",
+    choices: ["USD", "EUR", "GBP", "INR", "PKR"]
+},
+{
+    name: "tocurrency",
+    type: "list",
+    message: "Enter the currency that you want to change to",
+    choices: ["USD", "EUR", "GBP", "IND", "PKR"]
+}, {
+
+    name: "amount",
+    type: "number",
+    message: "Enter your amount"
+}
+]
+);
+let fromAmount = currency[useranswer.fromcurrency]
+let toAmount = currency[useranswer.tocurrency]
+let amount = useranswer.amount
+let baseAmount = amount / fromAmount
+let convertedAmount = baseAmount * toAmount
+console.log(convertedAmount)
